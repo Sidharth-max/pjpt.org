5dc1b8db67628174a0bf6455706fb5977b7bbf03
Sidharth Falodia <189434527+Sidharth-max@users.noreply.github.com>
2026-03-28 09:14:46 +0530

    fix

---
+++ server/scripts/migrateHttpToHttps.js
@@ -0,0 +1,23 @@
+import '../config/env.js';
+import mongoose from 'mongoose';
+import Image from '../models/Image.js';
+
+const MONGO_URI = process.env.MONGO_URI;
+
+const run = async () => {
+  await mongoose.connect(MONGO_URI);
+  console.log('Connected to MongoDB');
+
+  const result = await Image.updateMany(
+    { url: /^http:\/\// },
+    [{ $set: { url: { $replaceAll: { input: '$url', find: 'http://', replacement: 'https://' } } } }]
+  );
+
+  console.log(`Updated ${result.modifiedCount} image record(s).`);
+  await mongoose.disconnect();
+};
+
+run().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
